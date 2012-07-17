e529ea985762e152338a097b3b628eb835a42f58
Aaron D. Valade <adv@alum.mit.edu>
2012-07-17 17:55:12 +0800

    rev'ed version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "aaron@generalsensing.com"
 license          "Apache v2.0"
 description      "Configures sysctl parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.2.1"
 supports         "ubuntu"
 supports         "debian"
